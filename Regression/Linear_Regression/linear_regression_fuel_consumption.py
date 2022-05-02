78507504e01df65249f3794c9cf7efcb54770cc2
Charu <charudalu@gmail.com>
2022-05-02 22:05:59 +0100

    "polynomial regression"

--- Regression/Linear_Regression/linear_regression_fuel_consumption.py
+++ Regression/Linear_Regression/linear_regression_fuel_consumption.py
@@ -3,6 +3,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import warnings
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
 warnings.filterwarnings('ignore')
 
 # import data
@@ -13,11 +16,9 @@ fueleconomy_df = pd.read_csv('FuelEconomy.csv')
 X = fueleconomy_df[['Horse Power']]
 # save y in series
 y = fueleconomy_df['Fuel Economy (MPG)']
-from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
 # train the model
-from sklearn.linear_model import LinearRegression
 regressor = LinearRegression(fit_intercept=True)
 regressor.fit(X_train, y_train)
 
@@ -27,15 +28,15 @@ print('Linear Model Coeff (b):', regressor.intercept_)
 # test the model
 y_predict = regressor.predict(X_test)
 
-plt.scatter(X_train, y_train, color = 'gray')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.scatter(X_train, y_train, color='gray')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
 plt.xlabel('Horse Power (HP)')
 plt.ylabel('MPG')
 plt.title('HP vs. MPG (Training Set)')
 plt.show()
 
-plt.scatter(X_test, y_test, color = 'gray')
-plt.plot(X_test, regressor.predict(X_test), color = 'blue')
+plt.scatter(X_test, y_test, color='gray')
+plt.plot(X_test, regressor.predict(X_test), color='blue')
 plt.xlabel('Horse Power (HP)')
 plt.ylabel('MPG')
 plt.title('HP vs. MPG (Testing Set)')
@@ -43,7 +44,7 @@ plt.show()
 
 HP = 500
 HP = np.asarray(HP)
-HP = np.reshape(HP,(-1,1))
+HP = np.reshape(HP, (-1, 1))
 
 MPG = regressor.predict(HP)
 MPG
--- Regression/Linear_Regression/linear_regression_ice_cream.py
+++ Regression/Linear_Regression/linear_regression_ice_cream.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import warnings
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 warnings.filterwarnings('ignore')
 
 # Import data
@@ -16,30 +18,29 @@ y = IceCream['Revenue']
 # save x in dataframe
 X = IceCream[['Temperature']]
 
-from sklearn.model_selection import train_test_split
+# Create train and test data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 
 # train the model
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression(fit_intercept = True)
-regressor.fit(X_train,y_train)
+regressor = LinearRegression(fit_intercept=True)
+regressor.fit(X_train, y_train)
 
 print('Linear Model Coefficient (m): ', regressor.coef_)
 print('Linear Model Coefficient (b): ', regressor.intercept_)
 
 # test the model (output will be in numpy array format)
-y_predict = regressor.predict( X_test)
+y_predict = regressor.predict(X_test)
 
-plt.scatter(X_train, y_train, color = 'gray')
-plt.plot(X_train, regressor.predict(X_train), color = 'red')
+plt.scatter(X_train, y_train, color='gray')
+plt.plot(X_train, regressor.predict(X_train), color='red')
 plt.ylabel('Revenue [dollars]')
 plt.xlabel('Temperature [degC]')
 plt.title('Revenue Generated vs. Temperature @Ice Cream Stand(Training dataset)')
 # plt.show()
 
 # VISUALIZE TEST SET RESULTS
-plt.scatter(X_test, y_test, color = 'gray')
-plt.plot(X_test, regressor.predict(X_test), color = 'red')
+plt.scatter(X_test, y_test, color='gray')
+plt.plot(X_test, regressor.predict(X_test), color='red')
 plt.ylabel('Revenue [dollars]')
 plt.xlabel('Hours')
 plt.title('Revenue Generated vs. Hours @Ice Cream Stand(Test dataset)')
@@ -47,7 +48,7 @@ plt.show()
 
 T = [35]
 T = np.asarray(T)
-T = np.reshape(T,(-1,1))
+T = np.reshape(T, (-1, 1))
 
 y_predict = regressor.predict(T)
 print(y_predict)
